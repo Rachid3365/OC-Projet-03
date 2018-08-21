f8c942d17db4912c76b9ca6558a6eae66ba58cfb
Rachid3365 <rachidchakir@gmail.com>
2018-08-21 20:41:33 +0200

    Initial Commit

---
+++ OC Projet 03/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  OC Projet 03
+//
+//  Created by Rachid C on 21/08/2018.
+//  Copyright © 2018 Rachid Chakir. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
